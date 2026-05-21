b174c954de98b0aba4ad098c8142d36b801dcae6
ELITE-UG <labtekindustridasar@my.gunadarma.ac.id>
2026-05-21 16:14:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,30 @@
 import streamlit as st
+import pandas as pd
 
 # Judul aplikasi
-st.title("Aplikasi Sederhana Streamlit")
+st.title("Aplikasi Data Mahasiswa")
 
-# Input nama
-nama = st.text_input("Masukkan nama kamu:")
+# Input data
+nama = st.text_input("Masukkan Nama")
+nilai = st.number_input("Masukkan Nilai", 0, 100)
 
-# Tombol
-if st.button("Tampilkan"):
-    st.success(f"Halo, {nama}! Selamat belajar Streamlit 🚀")
+# Simpan data sementara
+if "data" not in st.session_state:
+    st.session_state.data = []
+
+# Tombol tambah data
+if st.button("Tambah Data"):
+    st.session_state.data.append({
+        "Nama": nama,
+        "Nilai": nilai
+    })
+
+# Tampilkan tabel
+if st.session_state.data:
+    df = pd.DataFrame(st.session_state.data)
+
+    st.subheader("Data Mahasiswa")
+    st.dataframe(df)
+
+    rata = df["Nilai"].mean()
+    st.success(f"Rata-rata nilai: {rata:.2f}")
