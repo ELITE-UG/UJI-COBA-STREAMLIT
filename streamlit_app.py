31437c97c30aba7d9d0c2bd022b7107074c0e590
ELITE-UG <labtekindustridasar@my.gunadarma.ac.id>
2026-05-21 16:20:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,5 @@
 import streamlit as st
+import math as mt
 import pandas as pd
 
-# Judul aplikasi
-st.title("Aplikasi Data Mahasiswa")
-
-# Input data
-nama = st.text_input("Masukkan Nama")
-nilai = st.number_input("Masukkan Nilai", 0, 100)
-
-# Simpan data sementara
-if "data" not in st.session_state:
-    st.session_state.data = []
-
-# Tombol tambah data
-if st.button("Tambah Data"):
-    st.session_state.data.append({
-        "Nama": nama,
-        "Nilai": nilai
-    })
-
-# Tampilkan tabel
-if st.session_state.data:
-    df = pd.DataFrame(st.session_state.data)
-
-    st.subheader("Data Mahasiswa")
-    st.dataframe(df)
-
-    rata = df["Nilai"].mean()
-    st.success(f"Rata-rata nilai: {rata:.2f}")
+st.title ("Menghitung Volume Tabung")
